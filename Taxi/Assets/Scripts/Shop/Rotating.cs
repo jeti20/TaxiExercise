becc71881e279a5fa372c0ad63b447d31d8ef7d8
Dehydrogenase <michalrodak98@gmail.com>
2023-09-15 17:24:15 +0200

    Shop scene + changing cards with buttons

---
+++ Taxi/Assets/Scripts/Shop/Rotating.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotating : MonoBehaviour
+{
+    public float rotationSpeed = 50.0f; // Prêdkoœæ obrotu w stopniach na sekundê.
+
+    void Update()
+    {
+        // Oblicz k¹t obrotu na podstawie prêdkoœci i czasu od ostatniej klatki.
+        float rotationAmount = rotationSpeed * Time.deltaTime;
+
+        // Obróæ cylindra wokó³ jego osi Y (pionowej) o obliczony k¹t.
+        transform.Rotate(Vector3.up, rotationAmount);
+    }
+}
---
+++ Taxi/Assets/Scripts/Shop/TeleportCars.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportCars : MonoBehaviour
+{
+    [SerializeField] private  Vector3 finalPosition;
+
+    private Vector3 initialPosition;
+
+    private void Awake()
+    {
+        initialPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, finalPosition, 0.1f);
+    }
+
+    private void OnDisable()
+    {
+        transform.position = initialPosition;
+    }
+}
